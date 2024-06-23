9aec06b671f0337b01a456f5756696439a9cdb88
Nathan Orgera <norgera@uwo.ca>
2024-06-22 23:40:56 -0400

    Update LinearNode.java

--- src/LinearNode.java
+++ src/LinearNode.java
@@ -1,69 +1,39 @@
 /**
- * LinearNode represents a node in a linked list
+ * LinearNode represents a node in a linked list.
  */
-public class LinearNode<E>
-{
+public class LinearNode<E> {
     private LinearNode<E> next;
     private E element;
     
-    /**
-     * Creates an empty node.
-     */
-    public LinearNode()
-    {
+    // Creates an empty node.
+    public LinearNode() {
         next = null;
         element = null;
     }
     
-    /**
-     * Creates a node storing the specified element.
-     *
-     * @param elem  the element to be stored within the new node
-     */
-    public LinearNode (E elem)
-    {
+    // Creates a node with the given element.
+    public LinearNode(E elem) {
         next = null;
         element = elem;
     }
     
-    /**
-     * Returns the node that follows this one.
-     *
-     * @return  the node that follows the current one
-     */
-    public LinearNode<E> getNext()
-    {
+    // Gets the next node.
+    public LinearNode<E> getNext() {
         return next;
     }
     
-    /**
-     * Sets the node that follows this one.
-     *
-     * @param node  the node to be set to follow the current one
-     */
-    public void setNext (LinearNode<E> node)
-    {
+    // Sets the next node.
+    public void setNext(LinearNode<E> node) {
         next = node;
     }
     
-    /**
-     * Returns the element stored in this node.
-     *
-     * @return  the element stored in this node
-     */
-    public E getElement()
-    {
+    // Gets the element stored in the node.
+    public E getElement() {
         return element;
     }
     
-    /**
-     * Sets the element stored in this node.
-     *
-     * @param elem  the element to be stored in this node
-     */
-    public void setElement (E elem)
-    {
+    // Sets the element stored in the node.
+    public void setElement(E elem) {
         element = elem;
     }
 }
-
