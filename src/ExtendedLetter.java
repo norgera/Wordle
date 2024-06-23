c76fe8b31bf9e90617b6801359cb258bb3944e5c
Nathan Orgera <norgera@uwo.ca>
2024-06-22 23:42:32 -0400

    clean

--- src/ExtendedLetter.java
+++ src/ExtendedLetter.java
@@ -1,85 +1,58 @@
-
 public class ExtendedLetter extends Letter {
-	
-	private String content;
+
+    private String content;
     private int family;
     private boolean related;
     private final int SINGLETON = -1;
 
-    /**
-     * constructor for ExtendedLetter
-     * @param s (string)
-     */
-    public ExtendedLetter(String s){
+    // Constructor for ExtendedLetter with string parameter
+    public ExtendedLetter(String s) {
         super('a');
         this.content = s;
         this.related = false;
         this.family = this.SINGLETON;
     }
 
-    /**
-     * other constructor for ExtendedLeter including new parameter "fam"
-     * @param s (string)
-     * @param fam (family)
-     */
-    public ExtendedLetter(String s, int fam){
+    // Constructor for ExtendedLetter with string and family parameters
+    public ExtendedLetter(String s, int fam) {
         super('a');
         this.content = s;
         this.related = false;
         this.family = fam;
     }
 
-    /**
-     * equals method to test if both family variables and content are equal
-     * @return True/False depending if they are equal.
-     */
-    public boolean equals(Object other){
-    	
-        if (other instanceof ExtendedLetter){
+    // Checks if both family variables and content are equal
+    public boolean equals(Object other) {
+        if (other instanceof ExtendedLetter) {
             ExtendedLetter otherExtendedLetter = (ExtendedLetter) other;
-            
-            if(this.family == otherExtendedLetter.family){
+            if (this.family == otherExtendedLetter.family) {
                 this.related = true;
             }
-            if(this.content == otherExtendedLetter.content){
-                return true;
-            }
+            return this.content.equals(otherExtendedLetter.content);
         }
         return false;
     }
 
-    /**
-     * toString method to display a new letter decorator in the condition that a letter is unused and related is true
-     */
-    public String toString(){
-    	if ((this.isUnused() == true) && (this.related == true)) {
+    // Displays a new letter decorator if unused and related is true
+    public String toString() {
+        if (this.isUnused() && this.related) {
             return "." + this.content + ".";
         }
         return decorator() + this.content + decorator();
     }
-    
 
-    /**
-     * fromString method that creates an array of Letter objects of the same size as param "content"
-     * @param content (array)
-     * @param codes (array)
-     * @return letters (array of letters)
-     */
-    public static Letter[] fromStrings(String[] content, int[] codes){
-    	
+    // Creates an array of Letter objects from given arrays of content and codes
+    public static Letter[] fromStrings(String[] content, int[] codes) {
         Letter[] letters = new Letter[content.length];
-        
-        if(codes == null){
-            for(int i = 0; i < letters.length; i++){
+        if (codes == null) {
+            for (int i = 0; i < letters.length; i++) {
                 letters[i] = new ExtendedLetter(content[i]);
             }
-        }else{
-            for(int i = 0; i  < content.length; i++){
+        } else {
+            for (int i = 0; i < content.length; i++) {
                 letters[i] = new ExtendedLetter(content[i], codes[i]);
             }
         }
         return letters;
     }
-
-
 }
